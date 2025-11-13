4a48d524662714de99429e6f574d04a63e48cd5a
Sendiko <sarangtawon897@gmail.com>
2025-11-13 09:11:08 +0700

    feat: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS on production/tunnel
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
